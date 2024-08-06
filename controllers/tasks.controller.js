2c5ec20ae834a31bef0e3e9d62ab2d72803ef962
Tomas <tramosyahari@gmal.com>
2024-08-06 11:55:46 -0300

    ya esta creado el get para poder ver las listas

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -19,6 +19,26 @@ const getAllTasks = (req, res) => {
     });
 };
 
+const getOnetask = (req, res) =>{
+    const conexion = getConnection();
+    conexion.connect(err => {
+        if (err) {
+            console.error('Error conectando a la base de datos:', err);
+            return res.status(500).send('Error conectando a la base de datos');
+        }
+
+        conexion.query('SELECT * FROM tasks WHERE id = ?', (err, results) => {
+            if (err) {
+                console.error('Error al obtener esta tarea:', err);
+                return res.status(500).send('Error al obtener las tareas');
+            }
+            res.json(results);
+            conexion.end();
+        });
+    })
+}
+
 module.exports = {
     getAllTasks,
+    getOnetask,
 }
--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const {
     getAllTasks,
+    getOnetask,
 } = require('../controllers/tasks.controller');
 
 router.get('/tasks', getAllTasks);
 
+router.get('/tasks/:id', getOnetask)
 module.exports = router;
