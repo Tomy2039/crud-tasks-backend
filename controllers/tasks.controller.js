e20da026c53c01f1fab721c485c815b7530e48c1
Tomas <tramosyahari@gmal.com>
2024-08-07 00:09:07 -0300

    ya esta creado el put y sus validaciones

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,3 +1,4 @@
+const conexion = require('../db/database');
 const getConnection = require('../db/database');
 
 const getAllTasks = (req, res) => {
@@ -68,8 +69,40 @@ const postOnetask = (req, res) => {
     })
 }
 
+const putOnetask = (req, res) => {
+    const conexion = getConnection();
+    const { id } = req.params;
+    const { title, description, isComplete } = req.body;
+
+    conexion.connect(err => {
+        if (err) {
+            console.error('Error conectando a la base de datos:', err);
+            return res.status(500).send('Error conectando a la base de datos');
+        }
+
+        const values = [title, description, isComplete, id]
+        conexion.query('UPDATE tasks SET title = ?, description = ?, isComplete = ? WHERE id = ?', values, (err, results) =>{
+            if (err) {
+                console.error('Error al actualizar esta tarea:', err);
+                return res.status(500).send('Error al crear las tareas');
+            } else {
+                return res.status(200).send('Tarea actualizada con exito');
+            }
+
+            if (results.length === 0) {
+                return res.status(404).send('tarea no encontrada')
+            }
+
+            res.send(results);
+            conexion.end();
+        })
+    })
+
+}
+
 module.exports = {
     getAllTasks,
     getOnetask,
     postOnetask,
+    putOnetask,
 }
--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -3,9 +3,12 @@ const {
     getAllTasks,
     getOnetask,
     postOnetask,
+    putOnetask,
 } = require('../controllers/tasks.controller');
 
 router.get('/tasks', getAllTasks);
 router.get('/tasks/:id', getOnetask);
 router.post('/tasks', postOnetask);
+router.put('/tasks/:id', putOnetask);
+
 module.exports = router;
